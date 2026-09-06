9531c61de0678ae491c8801d6f9de93a95ebc4cf
IAmAdityaSrivastava <adityasri1004.as@gmail.com>
2026-09-07 01:02:31 +0530

    lab-1

---
+++ Backend/Lab1/index.js
@@ -0,0 +1,11 @@
+import EventEmitter from "node:events";
+
+const myEmitter = new EventEmitter();
+myEmitter.on("greet",(teacher)=>{
+    console.log(`class started by ${teacher}`);
+});
+myEmitter.on("exit",(teacher)=>{
+    console.log(`class finished by ${teacher}`);
+});
+myEmitter.emit("greet","Mr. Chandrahas");
+myEmitter.emit("exit","Mr. Chandrahas");
